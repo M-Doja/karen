35bf9670380f713ba767801da6089b815542242f
M-Doja <micdoja@yahoo.com>
2016-03-19 14:12:49 -0700

    initial commit

---
+++ public/javascript/controllers/HomeController.js
@@ -0,0 +1,18 @@
+(function() {
+	'use strict';
+	angular.module('app')
+	.controller('HomeController', HomeController);
+
+	HomeController.$inject = [];
+
+	function HomeController(HomeFactory) {
+		var vm = this;
+		vm.title = 'WRITING RENOVATIONS';
+		vm.user = {}
+
+
+
+
+
+	}
+})();
